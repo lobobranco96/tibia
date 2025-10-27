b6960bfea55c14983037ca2d6beee94ef34d665b
lobobranco96 <rntt2511@gmail.com>
2025-10-27 17:52:00 +0000

    feat extra and skills silver layer

---
+++ mnt/src/jobs/silver/skills.py
@@ -0,0 +1,9 @@
+from src.jobs.utility import create_spark_session
+from pyspark.sql import functions as F
+from datetime import datetime
+import logging
+import sys
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+def silver_vocation(spark, date_str=None):
